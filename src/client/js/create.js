8e650b4cc91475a4888ac60f9dbd6758a76cb341
June Park <digitalonnet@digitalonnet-ui-MacBook-Pro.local>
2021-12-16 23:58:52 +0900

    Feat: selectbox toggle, study type, langtype 구현

--- src/client/js/create.js
+++ src/client/js/create.js
@@ -1,16 +1,86 @@
-const datePickerElement = document.querySelector(".date-picker");
-const selectedDateElement = document.querySelector(".date-picker .selected-date");
-const datesElement = document.querySelector(".date-picker .dates");
-const monthElement = document.querySelector(".date-picker .dates .month .mth");
-const nextMonthElement = document.querySelector(".date-picker .dates .month .next-month");
-const prevMonthElement = document.querySelector(".date-picker .dates .month .prev-month");
-const daysElement = document.querySelector(".date-picker .dates .days");
+// SELECTBOX CONTROL
+const selectBoxType = document.querySelector(".select-box.type");
+const selectBoxStacks = document.querySelector(".select-box.stacks");
+const selectionsType = document.querySelector(".selection-container.type");
+const selectionsLanguage = document.querySelector(".selection-container.language");
 
-const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "November", "December"];
+selectBoxType.addEventListener("click", toggleSelectBoxType);
+selectBoxStacks.addEventListener("click", toggleSelectBoxStack);
+
+function toggleSelectBoxType(event){
+    event.preventDefault();
+    selectionsType.classList.toggle("active");
+}
+function toggleSelectBoxStack(event){
+    event.preventDefault();
+    selectionsLanguage.classList.toggle("active");
+}
+
+// SELECTNOX CONTROL //
+
+// SELECT STUDY TAG
+const studyType = ["면접", "프로젝트", "스터디"];
+const stackType = ["HTML", "CSS", "JavaScript", "Node.JS", "React", "Python"];
+const studyTags = document.querySelector(".tags.study-type");
+const stackTags = document.querySelector(".tags.stack-type");
+
+function addStudyType(){ 
+    studyType.forEach((item) => {
+        const li = document.createElement("li");
+        const button = document.createElement("button");
+        li.classList.add("stacks")
+        button.textContent = item;
+        button.classList.add("selections")
+        selectionsType.appendChild(li).appendChild(button);
+
+    });
+}
+addStudyType();
+
+function addStackType(){ 
+    stackType.forEach((item) => {
+        const li = document.createElement("li");
+        const button = document.createElement("button");
+        li.classList.add("languages")
+        button.textContent = item;
+        button.classList.add("selections"); 
+        selectionsLanguage.appendChild(li).appendChild(button);
+
+    });
+}
+addStackType();
+
+
+const selectTypeParent = document.querySelector(".select-box.parent"); 
+
+selectionsType.addEventListener("click", (event) => { 
+
+    if (event.target.nodeName === "BUTTON") {
+        const li = document.createElement("li");
+        studyTags.appendChild(li);
+        event.preventDefault();
+        
+        const tag = document.querySelector(".tags.study-type li");
+        tag.textContent = `${event.target.textContent}`;
+    }
+    selectionsType.classList.remove("active");
+}); 
+
+// DATE PICKER
+// 시작일
+const datePickerElement = document.querySelector(".date.start .date-picker");
+const selectedDateElement = document.querySelector(".date.start .date-picker .selected-date");
+const datesElement = document.querySelector(".date.start .date-picker .dates");
+const monthElement = document.querySelector(".date.start .date-picker .dates .month .mth");
+const nextMonthElement = document.querySelector(".date.start .date-picker .dates .month .next-month");
+const prevMonthElement = document.querySelector(".date.start .date-picker .dates .month .prev-month");
+const daysElement = document.querySelector(".date.start .date-picker .dates .days");
+
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 let date = new Date();
 let day = date.getDate();
-let month = date.getMonth()-2;
+let month = date.getMonth();
 let year = date.getFullYear();
 
 let selectedDate = date;
@@ -18,30 +88,77 @@ let selectedDay = day;
 let selectedMonth = month;
 let selectedYear = year;
 
-monthElement.textContent = months[month] + ' ' + year;
-nextMonthElement.addEventListener('click', goToNextMonth());
- 
+monthElement.textContent = months[month] + " " + year;
+selectedDateElement.textContent = formatDate(date);
+selectedDateElement.dataset.value = selectedDate;
 
-// EVENT LISTNERS
+populateDates();
+
+nextMonthElement.addEventListener("click", goToNextMonth);
+prevMonthElement.addEventListener("click", goToPrevMonth);
 datePickerElement.addEventListener("click", toggleDatePicker);
-
 
 // FUNCTIONS
 function toggleDatePicker(event) {
     if (!checkEventPathForClass(event.path, "dates" )){
         datesElement.classList.toggle("active");
-    } 
+    }
 }
 
-function goToNextMonth(event){ 
+function goToNextMonth(event){
     month++;
     if (month > 11){
         month = 0;
         year++;
     }
     monthElement.textContent = months[month] + " " + year;
+    populateDates();
 }
 
+function goToPrevMonth(event){
+    month--;
+    if (month < 0){
+        month = 11;
+        year--;
+    }
+    monthElement.textContent = months[month] + " " + year;
+    populateDates();
+}
+
+function populateDates(event) {
+    daysElement.innerHTML = "";
+    let amountDays = 31;
+
+    if (month == 1){
+        amountDays = 28;
+    }
+
+    for(let i = 0; i < amountDays; i++) {
+        const dayElement = document.createElement("div");
+        dayElement.classList.add("day");
+        dayElement.textContent = i + 1;
+
+        if (selectedDay == (i + 1) && selectedYear == year && selectedMonth == month){
+            dayElement.classList.add("selected");
+        }
+
+        dayElement.addEventListener("click", function() {
+            selectedDate = new Date(year + "-" + (month + 1) + "-" + (i + 1));
+            selectedDay = (i + 1);
+            selectedMonth = month;
+            selectedYear = year;
+            
+            selectedDateElement.textContent = formatDate(selectedDate);
+            selectedDateElement.dataset.value = selectedDate;
+
+            populateDates();
+        });
+
+        daysElement.appendChild(dayElement);
+    }
+}
+
+// helper function
 function checkEventPathForClass(path, selector) {
     for(i=0; i < path.length; i++){
         if (path[i].classList && path[i].classList.contains(selector)) {
@@ -50,3 +167,32 @@ function checkEventPathForClass(path, selector) {
     }
     return false;
 }
+
+function formatDate(d) {
+    let day = d.getDate();
+    if (day < 10){
+        day = "0" + day;
+    }
+
+    let month = d.getMonth() + 1;
+    if (month < 10){
+        month = "0" + month;
+    }
+
+    let year = d.getFullYear();
+
+    return month + " / " + day + " / " + year;
+}
+
+// DATE PICKER //
+
+// LOCATION PICKER
+const selectBoxLocation = document.querySelector(".select-box.location");
+const selectionsLocation = document.querySelector(".selection-location");
+
+selectBoxLocation.addEventListener("click", toggleSelectBoxLocation);
+
+function toggleSelectBoxLocation(event){
+    event.preventDefault();
+    selectionsLocation.classList.toggle("active");
+} 
