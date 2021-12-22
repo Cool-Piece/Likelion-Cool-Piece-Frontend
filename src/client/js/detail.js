f74a6a3b77e9c86da6ef638e11653cd07327678a
Luceta <57941164+Luceta@users.noreply.github.com>
2021-12-22 15:06:55 +0900

    Fix: wepback bundle error about the create.html ,css ,js

---
+++ src/client/js/detail.js
@@ -0,0 +1,9 @@
+import "../scss/styles.scss";
+import heart from "../assets/image/heart_icon.png";
+import bookmark from "../assets/image/bookmark_icon.png";
+
+const heartIcon = document.querySelector(".heart-icon");
+heartIcon.src = heart;
+
+const bookmarkIcon = document.querySelector(".bookmark-icon");
+bookmarkIcon.src = bookmark;
