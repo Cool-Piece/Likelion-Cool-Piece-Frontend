d13fa8c7cbdf9e0d851d68c240caafb0f6e6bc2a
Luceta <57941164+Luceta@users.noreply.github.com>
2021-12-30 16:44:29 +0900

    Fix: merge but from jiyun branch styles.css.map

--- src/client/js/githubCallback.js
+++ src/client/js/githubCallback.js
@@ -7,25 +7,26 @@ logoBox.src = logos;
 
 const sendToken = async () => {
   // TODO: 배포용 서버로 fetch 바꿔주기!
-  const result = await fetch("http://localhost:5000/users/github/callback", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      code: new URL(window.location.href).searchParams.get("code"),
-    }),
-  });
+  const tokenRequest = await fetch(
+    "http://localhost:5000/users/github/callback",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        code: new URL(window.location.href).searchParams.get("code"),
+      }),
+    }
+  );
 
-  const test = await result.json();
-  console.log(test, "Test");
+  const result = await tokenRequest.json();
 
-  // TODO:  client에서 쿠키 저장해줄 것!
-  /* maxAge: 1d
-  if (result.ok) {
-    window.location.href = "http://127.0.0.1:5500/assets/html/index.html";
-    document.cookie = `jwt=${result.token};`;
-  }*/
+  if (result.message === "ok") {
+    window.location.href =
+      "http://127.0.0.1:5500/Likelion-Cool-Piece-Frontend/assets/html/index.html";
+    document.cookie = `access_token=${result.access_token}; max-age=246060;`;
+  }
 };
 
 sendToken();
