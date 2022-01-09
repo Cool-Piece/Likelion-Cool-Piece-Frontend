02b3dfb54270dc4d561e3478f91f14b74abe7119
junho0956 <junho0956@naver.com>
2022-01-09 16:02:48 +0900

    Style: add style detail page

--- src/client/js/mystudy.js
+++ src/client/js/mystudy.js
@@ -2,6 +2,8 @@ import "regenerator-runtime";
 import "../scss/styles.scss";
 import Auth from "./auth";
 import NavBar from "./navbar";
+import { BASE_URL } from './api';
+import { formatDate } from './utils';
 
 export default class MyStudy {
   data;
@@ -12,6 +14,25 @@ export default class MyStudy {
     // TODO: 북마크, 참여중인 스터디 데이터를 가져와주세요
     // 가져온 '스터디카드' 타입의 '배열' 데이터 fetchData를 setState에 넣어주시면 됩니다
     // this.setState(fetchData)
+    fetch(`${BASE_URL}/users/bookmark`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer${Auth.getToken()}`
+      }
+    })
+    .then(res => {
+      if (res.status === 200) {
+        return res.json()
+      } else {
+        return new Error('GET / 유저 북마크 에러');
+      }
+    })
+    .then(res => {
+      this.setState(res.user.bookmark);
+    })
+    .catch(err => {
+      console.error(err);
+    })
   }
 
   setState(nextState) {
@@ -22,7 +43,7 @@ export default class MyStudy {
   render() {
     this.$cardList.innerHTML = this.data.map((card) => {
       return `
-        <li class="study-card">
+        <li class="study-card" id=${card._id}>
           <div class="main-info">
               <p class="study-title">${card.title}</p>
               <p class="study-date">
@@ -34,10 +55,10 @@ export default class MyStudy {
               <ul class="tag-list">
                 ${card.skills.map((skill) => {
                   return `<li>${skill}</li>`;
-                })}
+                }).join('')}
               </ul>
               <!-- TODO: 참여기능 생기면 추가하기 -->
-              <!-- <div class="sutdy-status">참여중</div> -->
+              <div class="sutdy-status">참여중</div>
           </div>
           </li>
       `;
